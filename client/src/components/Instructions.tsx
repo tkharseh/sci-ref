632f57d9db9a685f5ab5f8061306f8b77c7ca2e9
mirzaahmadi <mamahmadi727@gmail.com>
2023-08-22 11:46:02 -0400

    Instructions updated

--- client/src/components/Instructions.tsx
+++ client/src/components/Instructions.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from 'react';
 
 const Instructions = () => {
   return (
-    <div>Instructions</div>
-  )
-}
+    <div className="text-center max-w-4xl mx-auto px-1 py-2">
+      <div>
+        <h1 className="font-sans text-black-600 font-bold text-xl">Instructions</h1>
+      </div>
+      <p className="font-sans text-black text-xl">
+        Arrange components by dragging and dropping to create your preferred reference order. Customize styles by ticking checkboxes for italics or bold. Complete the process by selecting the source repository, inputting the URL, and pressing enter to generate your tailored scientific reference.
+      </p>
+    </div>
+  );
+};
 
-export default Instructions
+export default Instructions;
