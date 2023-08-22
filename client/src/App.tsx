b07b458376e7903c38c72a0ad78e398f32a69f70
tkharseh <tariq.kharseh@mail.utoronto.ca>
2023-08-22 11:26:12 -0400

    installed mantine

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,16 @@
-import './App.css';
-import {ReactComponent as Logo} from './assets/logo.svg'
-import Instructions from './components/Instructions';
+import "./App.css";
+import { ReactComponent as Logo } from "./assets/logo.svg";
+import Instructions from "./components/Instructions";
+import { MantineProvider } from "@mantine/core";
 
 function App() {
   return (
-    <div className='grid justify-items-center'>
-      <Logo />
-      <Instructions />
-    </div>
+    <MantineProvider withGlobalStyles withNormalizeCSS>
+      <div className="grid justify-items-center">
+        <Logo />
+        <Instructions />
+      </div>
+    </MantineProvider>
   );
 }
 
