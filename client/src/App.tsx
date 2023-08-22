230ecbe699d4252437b5259cec4e32743e990ebe
tkharseh <tariq.kharseh@mail.utoronto.ca>
2023-08-22 11:06:22 -0400

    created instructions component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css';
 import {ReactComponent as Logo} from './assets/logo.svg'
+import Instructions from './components/Instructions';
 
 function App() {
   return (
     <div className='grid justify-items-center'>
       <Logo />
+      <Instructions />
     </div>
   );
 }
